d4bc757cfcfe978ada005da6617d2baf7c861570
killuclkhioo-boop <killuclkhioo@gmail.com>
2026-04-16 11:37:29 +0700

    Initialize package.json for web hosting control panel

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "web-hosting-control",
+  "version": "1.0.0",
+  "description": "Web Hosting Control Panel",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "child_process": "^1.0.2"
+  }
+}
